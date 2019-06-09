cb504c9e0a100b7c7dd939f830934df84cbc2048
u4bi <rootcode10@gmail.com>
2019-06-09 20:29:21 +0900

    feat: Added Section/MainSubContent component.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import './index.scss'
 
 import MainContent from './internal/Section/MainContent'
+import MainSubContent from './internal/Section/MainSubContent'
 import MainFeatures from './internal/Section/MainFeatures'
 
 export default class extends Component {
@@ -11,6 +12,7 @@ export default class extends Component {
             <div className='Main'>
 
                 <MainContent />
+                <MainSubContent />
                 <MainFeatures />
 
             </div>
---
+++ src/pages/Main/internal/Section/MainSubContent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default () => (
+    <section className='MainSubContent'>
+
+        <div className='erer__row'>
+
+            <div className='erer__col-12'>
+
+                <h5>SUB CONTENT TEST TITLE</h5>
+
+                <h2>
+                    메인 서브 콘텐츠의 테스트 타이틀입니다.
+                    <br /> 
+                    SUB CONTENT TEST & TITLE
+                    <br /> 
+                    이건 테스트입니다.
+                </h2>
+
+            </div>
+
+        </div>
+
+    </section>
+)
