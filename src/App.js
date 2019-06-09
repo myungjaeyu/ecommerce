fb033c4669d076a07a58eb708a2bdd69d6ad6d93
u4bi <rootcode10@gmail.com>
2019-06-09 16:47:22 +0900

    refactor: Code cleanup.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import './App.scss'
 
 import Main from './pages/Main'
-import Sub from './pages/Sub'
 
 import { Subscribe } from 'unstated'
 import { AppContainer } from './providers/containers'
@@ -15,14 +14,8 @@ class App extends Component {
             { (app) => (
                 <div className='App'>
 
-                    <span className='App__Title' onClick={ _ => app.setTitle('fast-cra-starter')
-                                                                    .then(_ => app.setPayloadContent('immutability-helper')) }>
-                        { app.state.title } - { app.state.payload.content }
-                    </span>
-
                     <Switch>
                         <Route exact path='/' component={ Main } />
-                        <Route exact path='/sub/:no' component={ Sub } />
                         <Redirect to='/'/>
                     </Switch>
 
--- src/components/Card.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Card.scss'
-
-export default ({ no }) => (
-    <div className='Card'>
-        <h5>Card { no }</h5>
-    </div>
-)
