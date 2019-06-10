b9a3bcd30acbd4a766054108c407754bcee68cb3
u4bi <rootcode10@gmail.com>
2019-06-10 11:06:53 +0900

    refactor: Animation improvements to SelectPopover component.

--- src/components/SelectPopover/SelectPopover.js
+++ src/components/SelectPopover/SelectPopover.js
@@ -5,19 +5,21 @@ import './SelectPopover.scss'
 export default class extends Component {
 
     state = { 
-        isPopover: false
+        show: false
     }
 
     handleIsVisiblePopover = () => {
 
-        this.setState(({ isPopover }) => ({ isPopover : !isPopover }))
+        this.setState(({ show }) => ({ show : !show }))
 
     }
 
-    paintPopover() {
+    paintPopover(show) {
+
+        const IS_SHOW_CLASSNAME = show ? 'SelectPopover__Open' : 'SelectPopover__Close'
 
         return (
-            <div className='SelectPopover__popover'>
+            <div className={ 'SelectPopover__popover ' + IS_SHOW_CLASSNAME }>
 
                 <button onClick={ this.handleIsVisiblePopover }>가격 높은 순</button>
                 <button onClick={ this.handleIsVisiblePopover }>가격 낮은 순</button>
@@ -33,7 +35,7 @@ export default class extends Component {
 
                 <button onClick={ this.handleIsVisiblePopover }>가격 높은 순</button>
 
-                { this.state.isPopover && this.paintPopover() }
+                { this.paintPopover(this.state.show) }
 
             </div>
         )
